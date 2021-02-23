2b58f4a042961d3f6319b01eda07785d160638ad
athithan jayabalakrishnan <athikrishnan5@gmail.com>
2021-02-23 07:24:55 +0530

    renamed project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/wms-design'),
+      dir: require('path').join(__dirname, './coverage/horizon'),
       subdir: '.',
       reporters: [
         { type: 'html' },

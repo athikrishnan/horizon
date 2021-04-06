1040f7c1c40a3831c75b40f18e8247d607a26f99
athithan jayabalakrishnan <athikrishnan5@gmail.com>
2021-04-06 08:05:06 +0530

    products now have code property

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,3 +8,7 @@ exports.onCustomerCreate = onCustomerCreate.onCustomerCreate;
 // invoice
 const onInvoiceCreate = require('./invoice/on-invoice-create.function');
 exports.onInvoiceCreate = onInvoiceCreate.onInvoiceCreate;
+
+// product
+const onProductCreate = require('./product/on-product-create.function');
+exports.onProductCreate = onProductCreate.onProductCreate;
---
+++ functions/src/models/product-doc.model.ts
@@ -0,0 +1,11 @@
+export interface ProductDoc {
+  id: string;
+  code: number;
+  name: string;
+  slab: any;
+  unit: any;
+  keywords: string[];
+  variants: any[];
+  createdAt: number;
+  updatedAt: number;
+}

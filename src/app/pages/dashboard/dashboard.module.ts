c7754a623a1cc274debeee5cfb3210e900babf40
athithan jayabalakrishnan <athikrishnan5@gmail.com>
2021-04-12 07:20:55 +0530

    added dashboard quick links

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashbord-routing.module';
-
-
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
